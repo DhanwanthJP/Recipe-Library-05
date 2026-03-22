71c9e1b68f0a80a50eeb711e054ab86b02d84b36
DhanwanthJP <dhanwanthoct18@gmail.com>
2026-03-22 16:12:21 +0530

    sideNav updated on all pages

--- add-recipe-page.js
+++ add-recipe-page.js
@@ -1,3 +1,15 @@
+// Side Nav
+const sideNav = document.getElementById("sideNav");
+const navbar_menu = document.getElementById("navbar-menu");
+navbar_menu.addEventListener("click", ()=>{
+  sideNav.style.marginLeft = 0;
+});
+
+const sideNav_close = document.getElementById("sideNav-close");
+sideNav_close.addEventListener("click", ()=>{
+  sideNav.style.marginLeft = `-50%`;
+});
+
 // Adding ingredient
 let ingCount = 0;
 
